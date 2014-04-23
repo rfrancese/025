6e493b179f7b3cff2f3845211d9a0b51e68f00a0
Maria Vittoria Coda <mavicoda@gmail.com>
2014-04-23 11:47:24 +0200

    modifiche MainActivity e layout

--- bdsir_takearound/src/it/unisa/bdsir_takearound/MainActivity.java
+++ bdsir_takearound/src/it/unisa/bdsir_takearound/MainActivity.java
@@ -1,7 +1,14 @@
 package it.unisa.bdsir_takearound;
 
 import android.app.Activity;
+import android.os.Bundle;
+import it.unisa.takearound.R;
 
 public class MainActivity extends Activity{
-
+	public void onCreate(Bundle icicle){
+		
+		super.onCreate(icicle);
+		
+		setContentView(R.layout.main);
+	}
 }
