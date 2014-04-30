69d1826cc3f7d4dca7be7cc02861a8fe3c462549
Maria Vittoria Coda <mavicoda@gmail.com>
2014-04-30 12:29:17 +0200

    Aggiunta di activity per Record e Tutorial, modifica dei layout per ogni activity, aggiornamento AndroidManifest.xml

--- bdsir_takearound/src/it/unisa/bdsir_takearound/MainActivity.java
+++ bdsir_takearound/src/it/unisa/bdsir_takearound/MainActivity.java
@@ -1,7 +1,11 @@
 package it.unisa.bdsir_takearound;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 import it.unisa.takearound.R;
 
 public class MainActivity extends Activity{
@@ -9,6 +13,42 @@ public class MainActivity extends Activity{
 		
 		super.onCreate(icicle);
 		
-		setContentView(R.layout.main);
+		setContentView(R.layout.main_activity);
+		
+		Button gioca = (Button) this.findViewById(R.id.buttonGioca);
+		gioca.setOnClickListener(new View.OnClickListener() {
+			
+			public void onClick(View v) {
+				Toast.makeText(getBaseContext(), "hai cliccato su GIOCA", Toast.LENGTH_SHORT).show();
+			}
+		});
+		
+		Button record = (Button) findViewById(R.id.buttonRecord);
+		record.setOnClickListener(new View.OnClickListener() {
+			
+			public void onClick(View v) {
+			//	Toast.makeText(getBaseContext(), "hai cliccato su Record", Toast.LENGTH_SHORT).show();
+				openRecordActivity(v);
+			}
+		});
+		
+		final Button tutorial = (Button) findViewById(R.id.buttonTutorial);
+		tutorial.setOnClickListener(new View.OnClickListener() {
+			
+			public void onClick(View v) {
+			//	Toast.makeText(getBaseContext(), "hai cliccato su Tutorial", Toast.LENGTH_SHORT).show();
+				openTutorialActivity(tutorial);
+			}
+		});
+	}
+	
+	protected void openRecordActivity(View view) {
+		Intent intent = new Intent(this, RecordActivity.class);
+		startActivity(intent);
+	}
+
+	protected void openTutorialActivity(View view){
+		Intent intent = new Intent(this, TutorialActivity.class);
+		startActivity(intent);
 	}
 }
