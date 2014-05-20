6d023eec554ec4d20afacbad7105b86e6381a579
Antonio Mautone <mauant@inwind.it>
2014-05-20 11:07:59 +0200

    ver 1.0 unisa
    Signed-off-by: Antonio Mautone <mauant@inwind.it>

--- bdsir_takearound/src/it/unisa/bdsir_takearound/MainActivity.java
+++ bdsir_takearound/src/it/unisa/bdsir_takearound/MainActivity.java
@@ -1,8 +1,56 @@
 package it.unisa.bdsir_takearound;
 
 import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+import it.unisa.takearound.R;
 
 public class MainActivity extends Activity{
+	public void onCreate(Bundle icicle){
+		
+		super.onCreate(icicle);
+		
+		setContentView(R.layout.main_activity);
+		
+		Button gioca = (Button) this.findViewById(R.id.buttonGioca);
+		gioca.setOnClickListener(new View.OnClickListener() {
+			
+			public void onClick(View v) {
+				Toast.makeText(getBaseContext(), "hai cliccato su GIOCA", Toast.LENGTH_SHORT).show();
+			}
+		});
+		
+		Button record = (Button) findViewById(R.id.buttonRecord);
+		record.setOnClickListener(new View.OnClickListener() {
+			
+			public void onClick(View v) {
+			//	Toast.makeText(getBaseContext(), "hai cliccato su Record", Toast.LENGTH_SHORT).show();
+				openRecordActivity(v);
+			}
+		});
+		
+		final Button tutorial = (Button) findViewById(R.id.buttonTutorial);
+		tutorial.setOnClickListener(new View.OnClickListener() {
+			
+			public void onClick(View v) {
+			//	Toast.makeText(getBaseContext(), "hai cliccato su Tutorial", Toast.LENGTH_SHORT).show();
+				openTutorialActivity(tutorial);
+			}
+		});
+	}
+	
+	protected void openRecordActivity(View view) {
+		Intent intent = new Intent(this, RecordActivity.class);
+		startActivity(intent);
+	}
 
-	//commento per push
+
+	protected void openTutorialActivity(View view){
+		Intent intent = new Intent(this, TutorialActivity.class);
+		startActivity(intent);
+	}
+//>>>>>>> branch 'master' of ssh://git@github.com/rfrancese/TakeARound
 }
---
+++ bdsir_takearound/src/it/unisa/bdsir_takearound/TutorialActivity.java
@@ -0,0 +1,14 @@
+package it.unisa.bdsir_takearound;
+
+import it.unisa.takearound.R;
+import android.app.Activity;
+import android.os.Bundle;
+
+public class TutorialActivity extends Activity {
+	
+	public void onCreate(Bundle icicle){
+		super.onCreate(icicle);
+		setContentView(R.layout.tutorial_activity);
+	}
+
+}
