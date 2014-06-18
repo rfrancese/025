ac7b2b5a51ba50e48a0a5e029770b866501a86b5
Maria Vittoria Coda <mavicoda@gmail.com>
2014-06-19 00:14:34 +0200

    aggiornati assets e LoadingScreen
    Signed-off-by: Maria Vittoria Coda <mavicoda@gmail.com>

--- bdsir_takearound/src/it/unisa/bdsir_takearound/game/LoadingScreen.java
+++ bdsir_takearound/src/it/unisa/bdsir_takearound/game/LoadingScreen.java
@@ -24,14 +24,7 @@ public class LoadingScreen extends Screen {
         Assets.ready = g.newPixmap("ready.png", PixmapFormat.ARGB4444);
         Assets.pause = g.newPixmap("pausemenu.png", PixmapFormat.ARGB4444);
         Assets.gameOver = g.newPixmap("gameover.png", PixmapFormat.ARGB4444);
-        Assets.headUp = g.newPixmap("headup.png", PixmapFormat.ARGB4444);
-        Assets.headLeft = g.newPixmap("headleft.png", PixmapFormat.ARGB4444);
-        Assets.headDown = g.newPixmap("headdown.png", PixmapFormat.ARGB4444);
-        Assets.headRight = g.newPixmap("headright.png", PixmapFormat.ARGB4444);
-        Assets.tail = g.newPixmap("tail.png", PixmapFormat.ARGB4444);
-        Assets.stain1 = g.newPixmap("stain1.png", PixmapFormat.ARGB4444);
-        Assets.stain2 = g.newPixmap("stain2.png", PixmapFormat.ARGB4444);
-        Assets.stain3 = g.newPixmap("stain3.png", PixmapFormat.ARGB4444);
+
         
         Assets.click = game.getAudio().newSound("click.ogg");
         Assets.eat = game.getAudio().newSound("eat.ogg");
